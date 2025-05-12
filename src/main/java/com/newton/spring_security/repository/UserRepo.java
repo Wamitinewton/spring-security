0fc8fe57f3c5feb5ef05582354ed62026f0e75ab
newton <wamitinewton@gmail.com>
2025-05-12 13:26:09 -0400

    changes

---
+++ src/main/java/com/newton/spring_security/repository/UserRepo.java
@@ -0,0 +1,11 @@
+package com.newton.spring_security.repository;
+
+import com.newton.spring_security.models.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, Integer> {
+
+    Users findByUsername(String username);
+}
